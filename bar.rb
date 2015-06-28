b328cfea46fa7009c58b78db969209ad8c436119
compwron <compiledwrong@gmail.com>
2015-06-28 00:09:11 -0700

    tabs to spaces because my editor settings dun broke

--- bar.rb
+++ bar.rb
@@ -1,56 +1,56 @@
 class Dictionary
-	def self.words(word_size=4)
-		@words ||= (puts 'Loading dictionary!' ; File.readlines('dict.txt').map{|i| i.strip}.select {|i| i.size == word_size })
-	end
+  def self.words(word_size=4)
+    @words ||= (puts 'Loading dictionary!' ; File.readlines('dict.txt').map{|i| i.strip}.select {|i| i.size == word_size })
+  end
 end
 
 class WordChain
-	def initialize(options)
-		@current = options[:current]
-		@end_word = options[:end_word]
-		@depth = (options[:depth] || 0) + 1
-		@max_depth = options[:max_depth]
-		@previous_words = (options[:previous_words] || []) + [@current]
-		@morphs = _morphs
-	end
+  def initialize(options)
+    @current = options[:current]
+    @end_word = options[:end_word]
+    @depth = (options[:depth] || 0) + 1
+    @max_depth = options[:max_depth]
+    @previous_words = (options[:previous_words] || []) + [@current]
+    @morphs = _morphs
+  end
 
-	def _morphs
-		words_for.map { |w|
-			if w == @end_word
-				puts "#{(@previous_words + [w]).join(' ')}"
-				return
-			elsif make_new_words?(w)
-				WordChain.new(_next_options(w))
-			end
-		}
-	end
+  def _morphs
+    words_for.map { |w|
+      if w == @end_word
+        puts "#{(@previous_words + [w]).join(' ')}"
+        return
+      elsif make_new_words?(w)
+        WordChain.new(_next_options(w))
+      end
+    }
+  end
 
-	def make_new_words?(w)
-		@depth < @max_depth && !@previous_words.include?(w)
-	end
+  def make_new_words?(w)
+    @depth < @max_depth && !@previous_words.include?(w)
+  end
 
-	def words_for
-		Dictionary.words.select{|w| almost?(@current, w.downcase)}.reject {|i| i == @current }.map(&:downcase).uniq
-	end
+  def words_for
+    Dictionary.words.select{|w| almost?(@current, w.downcase)}.reject {|i| i == @current }.map(&:downcase).uniq
+  end
 
-	def almost?(a, b)
-		a.split('').zip(b.split('')).select {|pair| pair.first != pair.last}.count == 1
-	end
+  def almost?(a, b)
+    a.split('').zip(b.split('')).select {|pair| pair.first != pair.last}.count == 1
+  end
 
-	def _next_options(w)
-		{
-			current: w,
-			end_word: @end_word,
-			depth: @depth,
-			max_depth: @max_depth,
-			previous_words: @previous_words
-		}
-	end
+  def _next_options(w)
+    {
+      current: w,
+      end_word: @end_word,
+      depth: @depth,
+      max_depth: @max_depth,
+      previous_words: @previous_words
+    }
+  end
 end
 
 if ![2, 3].include?(ARGV.size)
-	puts "Usage: \n 	<first word> <last word> <maximum depth>(optional)\n 	ruby bar.rb word bird 3\n 	ruby bar.rb hint bent 2\n 	ruby bar.rb gnat flat 3"
-	exit
+  puts "Usage: \n   <first word> <last word> <maximum depth>(optional)\n  ruby bar.rb word bird 3\n   ruby bar.rb hint bent 2\n   ruby bar.rb gnat flat 3"
+  exit
 end
 starter_word = ARGV[0]
 end_word = ARGV[1]
